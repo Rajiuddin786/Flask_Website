b2c60d83a321831366baa81d2970c945230bb2f4
Rajiuddin <skrajiuddin18@gmail.com>
2025-05-13 11:29:02 +0530

    "Use gridfs to store images in MongoDB"

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask,render_template,redirect,request,session
+from flask import Flask,render_template,redirect,request,session,send_file
 from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import os
+from io import BytesIO
+import gridfs
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 mongo_client = MongoClient("mongodb+srv://skrajiuddin18:Raj24032003@cluster0.i1pmhyj.mongodb.net/")
-db = mongo_client["admin_"]
-collection = db["admin_"]
-image_collection = db["images"]
+db1 = mongo_client["admin_"]
+collection = db1["admin_"]
+fs1=gridfs.GridFS(db1)
+db2= mongo_client["local_"]
+fs2=gridfs.GridFS(db2)
 
 
 @app.route("/")
 def home():
-    datas=image_collection.find()
-    return render_template("index.html",images=datas)
+    image_files1 = [
+        {"_id": str(image._id), "filename": image.filename}
+        for image in fs1.find()
+    ]
+    image_files2 = [
+        {"_id": str(image._id), "filename": image.filename,"title": image.title, "description": image.description}
+        for image in fs2.find()
+    ]
+    images = {"image_files1": image_files1,
+             "image_files2": image_files2}
+    return render_template("index.html",images=images)
 
 @app.route("/login")
 def login():
@@ -45,40 +58,54 @@ def submit():
 
 @app.route("/admin")
 def admin_page():
-    datas=image_collection.find()
     if collection.find_one({"username": session.get("username")}):
         if session.get("username"):
-            return render_template("admin.html",images=datas)
+            image_files = [
+        {"_id": str(image._id), "filename": image.filename,"title": image.title, "description": image.description}
+        for image in fs2.find()
+    ]
+            return render_template("admin.html",images=image_files)
         else:
             return redirect("/login")
     else:
         return redirect("/login")
 
-@app.route("/upload",methods=["POST"])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == "POST":
-        title = request.form["title"]
-        description = request.form["description"]
-        image = request.files["image"]
-        image.save(f"static/images/{image.filename}")
-        image_data = {
-            "title": title,
-            "description": description,
-            "image_path": f"static/images/{image.filename}"
-        }
-        image_collection.insert_one(image_data)
-        return redirect("/admin")
+    if 'image' not in request.files:
+        return "No file part"
+    image = request.files['image']
+    title = request.form['title']
+    description = request.form['description']
+    if image.filename == '':
+        return "No selected file"
+
+    file_id = fs2.put(image, filename=image.filename,title=title, description=description)
     return redirect("/admin")
+
         
 @app.route("/delete/<image_id>",methods=["POST"])
 def delete(image_id):
     if request.method == "POST":
-        image = image_collection.find_one({"_id": ObjectId(image_id)})
-        if image:
-            os.remove(image["image_path"])
-            image_collection.delete_one({"_id": ObjectId(image_id)})
-            return redirect("/admin")
+        image = fs2.get(ObjectId(image_id))
+        fs2.delete(image._id)
         return redirect("/admin")
+    
+@app.route('/image/<filename>')
+def index_image(filename):
+    image = fs1.find_one({'filename': filename})
+    if image:
+        return send_file(BytesIO(image.read()), mimetype='image/jpeg')
+    else:
+        return "Image not found", 404
+
+@app.route('/gallery/<image_id>')
+def image(image_id):
+    image = fs2.get(ObjectId(image_id))
+    if image:
+        return send_file(BytesIO(image.read()), mimetype='image/jpeg')
+    else:
+        return "Image not found", 404
 
 
 if __name__ == "__main__":
